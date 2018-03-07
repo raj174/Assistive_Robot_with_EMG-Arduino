fa2059b454328622eeb81f153f6666391f0d325b
Raj Manandhar <RM1348@live.mdx.ac.uk>
2018-03-07 12:42:12 +0000

    added on and off for rf module measuring EMG sensor activity

--- RF/Transmitt/Transmitt.ino
+++ RF/Transmitt/Transmitt.ino
@@ -2,17 +2,24 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(2, 3); // CE, CSN
+RF24 radio(3,4); // CE, CSN
 
 
 //const byte address[6] = "00001";
 const int SensorValue = A0;
+const int threshold = 900;
 const uint64_t address[] = {0x7878787878LL, 0xB3B4B5B6F1LL, 0xB3B4B5B6CDLL};
-int Value;
+int Value, previousValue;
+bool power = true;
+bool counter = true;
+volatile byte count;
+unsigned long previousMillis = 0; 
+const long interval = 5000;
 void setup()
 {
   radio.begin();
   Serial.begin(9600);
+  radio.setRetries(15,10);
   radio.openWritingPipe(address[0]);
   radio.openReadingPipe(0, address[3]);
   radio.setPALevel(RF24_PA_MIN);
@@ -21,10 +28,53 @@ void setup()
 
 void loop()
 {
+  unsigned long currentMillis = millis();
   Value = analogRead(SensorValue);
-  Serial.println(Value);
-  if(Value>900)
+  //Serial.println(Value);
+  if (currentMillis - previousMillis >= interval) //makes the rf module to sleep if the sensor value hasnt got any signal for a given amount of time
   {
-  radio.write(&Value, sizeof(Value));
+    previousMillis = currentMillis;
+    if (power == true)
+    {
+      Serial.print("stop");
+      radio.powerDown();
+      power = false;
+      count = 0;
+    }
   }
+
+  if(counter == false && previousValue <400){
+    counter = true;
+  }
+  previousValue = Value;
+  if(Value>threshold && count < 5 && power == false && counter == true) // To power up and start sending signals to receiver, need to clench fist five times 
+  {
+      count++;
+      Serial.println("counting");
+      //delay(1000);
+      counter = false;
+
+      
+  }
+  else if (count >= 5 && power == false) // if user clenched fist five times power up the rf module and restart the timer
+  {
+    Serial.println("turnon");
+    radio.powerUp();
+    power = true;
+    previousMillis = currentMillis;
+  }
+  
+  if (power == true) //if the rf module has powered up and if the sensor vlaue if above the threshold then send it to the receiver
+  { 
+    //Serial.println(Value);
+    //radio.write(&Value, sizeof(Value));
+    if(Value>threshold)
+    {
+      Serial.println("sending");
+      previousMillis = currentMillis;
+      radio.write(&Value, sizeof(Value));
+      delay(400);
+    }
+  }
+    
 }
--- RF/Transmitt2/Transmitt2.ino
+++ RF/Transmitt2/Transmitt2.ino
@@ -2,14 +2,15 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(2,3); // CE, CSN
+RF24 radio(3,4); // CE, CSN
 
 const uint64_t address[] = {0x7878787878LL, 0xB3B4B5B6F1LL, 0xB3B4B5B6CDLL};
-const int SensorValue = A3;
+const int SensorValue = A0;
 int Value;
 
 void setup() {
   radio.begin();
+  Serial.begin(9600);
   radio.openWritingPipe(address[1]);
   radio.openReadingPipe(0, address[0]);
   radio.setPALevel(RF24_PA_MIN);
